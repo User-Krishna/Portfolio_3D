67bd977783f92148e81f1f6b1db7261faf1f55e4
Krishna Das <dask84779@gmail.com>
2026-03-30 06:08:46 -0700

    Initial commit with updated project files

---
+++ src/assets/index.js
@@ -0,0 +1,86 @@
+import logo from "./logo.jpg";
+import backend from "./ML and Data Analyst.jpg";
+import creator from "./Database Designer.jpg";
+import mobile from "./UIUX.jpg";
+import web from "./java Fullstack.jpg";
+import github from "./github.png";
+import linkedIn from "./linkedIn.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import demo from "./demo.png";
+
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import java from "./tech/java.png";
+import mysql from "./tech/mysql.png";
+import reactjs from "./tech/reactjs.png";
+import nextJs from "./tech/nextjs.svg";
+import angular from "./tech/angular.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import crmnext from "./company/crmnext.png";
+import nickelfox from "./company/nickelfox.png";
+import newgen from "./company/newgen.png";
+import skillrisers from "./company/skillrisers.png";
+
+
+import aptihealthWeb from "./projects/aptihealthWeb.png";
+import aptihealthApp from "./projects/aptihealthApp.png";
+import zealWeb from "./projects/zealWeb.jpg";
+import zealApp from "./projects/zealApp.jpg";
+import rollWeb from "./projects/rollWeb.jpg";
+import rollApp from "./projects/rollApp.png";
+
+import cert1 from "./certificates/certificate-1.jpeg";
+import cert2 from "./certificates/certificate-2.jpg";
+import cert4 from "./certificates/certificate-4.jpg";
+import cert5 from "./certificates/certificate-5.jpeg";
+import cert6 from "./certificates/certificate-6.jpeg";
+
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  linkedIn,
+  demo,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  java,
+  mysql,
+  reactjs,
+  nextJs,
+  angular,
+  redux,
+  tailwind,
+  typescript,
+  crmnext,
+  nickelfox,
+  newgen,
+  skillrisers,
+  aptihealthWeb,
+  aptihealthApp,
+  zealWeb,
+  zealApp,
+  rollWeb,
+  rollApp,
+  cert1,
+  cert2,
+  cert4,
+  cert5,
+  cert6,
+};
